b13763e56b2cc6ec0dadb32e47a098f34895c9c9
work2544 <work21_123@hotmail.com>
2025-02-27 02:51:07 +0700

    add register

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -70,7 +70,7 @@ function Register() {
             Password
           </label>
           <input
-            className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className={`shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline ${errors.password? 'border-red-500':''}`}
             id="password"
             type="password"
             {...register("password", { required: true })}
@@ -87,7 +87,7 @@ function Register() {
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
           >
-            Log In
+            register
           </button>
         </div>
       </form>
